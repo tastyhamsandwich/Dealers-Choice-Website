c2ee279545714142dc301795d8d5984eff3dbb80
tastyhamsandwich <danvisibleman+github2@gmail.com>
2025-02-27 00:39:32 -0600

    Configuration updates
    Added proper tsconfig aliases, Updated packages and specified use with pnpm package manager

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ const config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
